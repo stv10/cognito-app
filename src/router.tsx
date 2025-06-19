57a48e61fa427bb32e1541383bf5c23fb16b81a5
stv10 <santiagoinbox1@gmail>
2025-06-19 18:36:27 -0300

    Remove unused authentication loader from router configuration

--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,6 @@ import HomePage from "./pages/HomePage";
 import DashboardRoute from "./components/DashboardRoute";
 import RootLayout from "./components/RootLayout";
 
-// Authentication loader for protected routes
-const authLoader = () => {
-  // This will be called before the route renders
-  // We'll handle auth checks in the component itself
-  return null;
-};
-
 // Create the router
 export const router = createBrowserRouter([
   {
@@ -23,7 +16,6 @@ export const router = createBrowserRouter([
       {
         path: "dashboard",
         element: <DashboardRoute />,
-        loader: authLoader,
       },
     ],
   },
